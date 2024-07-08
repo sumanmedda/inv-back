cac5f0788563791d40d729cfd84187982c746158
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2024-07-08 14:20:34 +0530

    created user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    is_Admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
+    
